3dab76fee312d86d558a81ffacc42d99a3e91f07
xhdong <dongxh@133.cn>
2021-04-21 10:43:22 +0800

    feat: canvas前端项目

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+// 引入router
+import router from './router'
+import 'script-loader!createjs/builds/1.0.0/createjs.min.js'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+  router,
+}).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+const Canvas = (resolve) => require(['@/views/Canvas/Index'], resolve)
+
+/* Router Modules */
+export const constantRoutes = [
+  { path: '/canvas', name: 'Canvas', component: Canvas }
+]
+
+export default new Router({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes
+})
+
+export const asyncRoutes = [
+  { path: '*', redirect: '/404', hidden: true }
+]
