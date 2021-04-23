155c7a3477749d960e0bf61987d5bdb58192cacd
nan.zhang <576274890@qq.com>
2021-04-23 23:37:55 +0800

    图片太多，用base64格式会降低页面性能

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -2,14 +2,13 @@
   <div ref="loading" id="loading" class="loading">
     <img ref=img src="../assets/images/loading.png" style="width: 100%;">
     <div style="position: absolute; top:46.7%; left: 14.6%; width: 44%; height: 1.7%;">
-        <img ref="progress" src="../assets/images/progress.png" style="width: 0%; height: 100%;display: block;">
+        <img ref="progress" src="../assets/images/progress.png" style="width: 0%; height: 100%;display: block;" v-show="showP">
     </div>
   </div>
 </template>
 
 <script>
-//   import { defaultConfig } from '../../utils/config'
-//   import { getResizeValue, getPixelRatio } from '../../utils/config'
+  import { Lib } from '../utils/lib.js'
   export default {
     name: "Loading",
     data () {
@@ -17,14 +16,58 @@
         el: null,
         img: null,
         progress: null,
+        showL: true,
+        showP: true,
+        lib: null,
+        loader: null,
+        images: null
       }
     },
     mounted() {
+      // this.showLoading()
       this.el = this.$refs.loading
       this.img = this.$refs.img
       this.progress = this.$refs.progress
+      this.lib = new Lib()
+      this.loadImg()
     },
     methods: {
+      // showLoading(isShowProgress) {
+      //   this.showL = true
+      //   this.showP = isShowProgress === undefined ? true : isShowProgress
+      // },
+      hideLoading () {
+        this.showL = false
+      },
+      loadImg() {
+        const manifest = this.lib.property.manifest
+
+        if(!manifest.length) {
+          this.onComplete(null)
+        } else {
+          this.images = this.images || {}
+          this.loader = new window.createjs.LoadQueue(true)
+          this.loader.installPlugin(window.createjs.Sound)
+          this.loader.addEventListener('complete', this.onComplete)
+          this.loader.addEventListener('fileload', this.onFileload)
+          // this.loader.loadManifest(manifest)
+        }
+      },
+      onComplete (event) {
+        console.log('event: ', event);
+        this.hideLoading()
+      },
+      onFileload (event) {
+        console.log('event: ', event);
+        const item = event.item
+        if(item.type === 'image') {
+          this.images[item.id] = event.result
+        }
+        this.setProgress(parseInt(this.loader.progress * 100))
+      },
+      setProgress (progress) {
+        this.progress.style.width = `${progress}%`
+      }
     }
   }
 </script>
@@ -37,6 +80,5 @@
     top: 0;
     left: 0;
     z-index: 1000;
-    display: none;
 }
 </style>
