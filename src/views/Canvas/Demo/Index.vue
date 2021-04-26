2510f28435ed7f3f70bf17d883d77c217c2d741d
xhdong <dongxh@133.cn>
2021-04-26 22:16:22 +0800

    feat: demo初实验

--- src/views/Canvas/Demo/Index.vue
+++ src/views/Canvas/Demo/Index.vue
@@ -12,8 +12,30 @@
 
       }
     },
+    mounted() {
+      this.onResize()
+      window.addEventListener('resize', this.onResize)
+    },
     methods: {
+      onResize() {
+        const { margin, width, height } = getResizeValue()
+        const ratio = getPixelRatio(this.context)
 
+        this.container.style.width = `${width}px`
+        this.container.style.height = `${height}px`
+        this.container.style.margin = margin
+
+        this.canvas.style.width = `${width}px`
+        this.canvas.width = width * ratio
+        this.canvas.style.height = `${height}px`
+        this.canvas.height = height * ratio
+
+        if(this.stage) {
+          this.stage.scaleX = width / this.defaultConfig.width * ratio
+          this.stage.scaleY = height / this.defaultConfig.height * ratio
+          this.stage.update()
+        }
+      }
     }
   }
 </script>
